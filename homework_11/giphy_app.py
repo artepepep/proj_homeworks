2793b26f182bf7ed06bd67296ba21126b608a04a
artepepep <artem1884@outlook.com>
2023-09-04 15:10:45 +0300

    update

--- homework_11/giphy_app.py
+++ homework_11/giphy_app.py
@@ -1,3 +1,16 @@
 import requests
-from data_1 import proggram
-proggram()
+with open('key.txt', 'r') as file:
+    GIPHY_KEY = file.read()
+GIPHY_URL = 'https://api.giphy.com/v1/gifs/search'
+user_input = input("search the gif:")
+resp = requests.get(
+    GIPHY_URL,
+    params={
+        'api_key': GIPHY_KEY,
+        'q': user_input,
+        "limit": 1
+    }
+)
+data_2 = resp.json()
+for gifs in data_2.get("data", []):
+    print (gifs['url'])
