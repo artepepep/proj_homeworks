8e5c29e3a9429d434bf89b4122c5e1014c026147
artepepep <artem1884@outlook.com>
2023-09-09 18:02:14 +0300

    new commit

---
+++ homework_13/task_2.py
@@ -0,0 +1,14 @@
+class Country:
+    def __init__(self, name: str, population: int):
+        self.name = name
+        self.population = population
+    def __add__(self, other):
+        new_country_name = f'{self.name} {other.name}'
+        new_country_population = self.population + other.population
+        new_country = Country(new_country_name, new_country_population)
+        return new_country
+bosnia = Country('Bosnia', 10_000_000)
+herzegovina = Country('Herzegovina', 5_000_000)
+bosnia_herzegovina = bosnia + herzegovina
+print(bosnia_herzegovina.name)
+print(bosnia_herzegovina.population)
