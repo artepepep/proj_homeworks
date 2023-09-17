777baac6f6fe235181803d1cc4eaa50838b527bf
artepepep <artem1884@outlook.com>
2023-09-17 14:10:46 +0300

    task_2 commit

--- homework_14/task_2.py
+++ homework_14/task_2.py
@@ -4,8 +4,20 @@ class Restaurant:
         self.cuisine = cuisine
         self.menu = menu
 class FastFood(Restaurant):
-    # your code here
-    pass
+    def __init__(self, name, cuisine, menu, drive_thru: bool):
+        super().__init__(name, cuisine, menu)
+        self.drive_thru = drive_thru
+    def order(self, dish_name, quantity):
+        if dish_name in self.menu:
+            dish_details = self.menu[dish_name]
+            if quantity <= dish_details['quantity']:
+                total_cost = dish_details['price'] * dish_details['quantity']
+                return total_cost
+            else:
+                return "Requested quantity not available"
+        else:
+            return "Dish not available"
+
 
 menu =  {
     'burger': {'price': 5, 'quantity': 10},
@@ -15,6 +27,6 @@ menu =  {
 
 mc = FastFood('McDonalds', 'Fast Food', menu, True)
 
-print(mc.order('burger', 5)) # 25
-print(mc.order('burger', 15)) # Requested quantity not available
-print(mc.order('soup', 5)) # Dish not available
+print(mc.order('burger', 5))
+print(mc.order('burger', 15))
+print(mc.order('soup', 5))
